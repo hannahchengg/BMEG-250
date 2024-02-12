8a150876e168aa1f44ab166cf5185779102e22ee
hannahchengg <hc200409@gmail.com>
2024-02-12 12:04:02 -0800

    question 1

---
+++ ODE solver.py
@@ -0,0 +1,22 @@
+# import libraries
+import numpy as np
+from scipy.integrate import odeint #Using the simple ODE solver
+import matplotlib.pyplot as plt
+
+#parameter values
+
+#Define System of ODEs
+def odesystem (y, x):
+    dydx = (x*x*y*y + y)/(2*x*x*x*y - x)
+    return dydx
+
+#solve ODE
+x = np.linspace(0, 20, 200)
+x0 = 0
+y0 = 0
+
+y = odeint(odesystem, y0, x)
+
+#plot results
+plt.plot(x,y)
+plt.show()
