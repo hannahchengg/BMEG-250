5455f0724e94a8ca9fc3d3f28b7a9791284a80b5
hannahchengg <hc200409@gmail.com>
2024-02-13 08:39:11 -0800

    Question 2

---
+++ System ODE solver.py
@@ -0,0 +1,29 @@
+# import libraries
+import numpy as np
+from scipy.integrate import odeint #Using the simple ODE solver
+import matplotlib.pyplot as plt
+
+#parameter values
+
+#Define System of ODEs
+def odesystem (z, t):
+    x = z[0]
+    y = z[1]
+    dxdt = y
+    dydt = -2*x-3*y
+    dzdt = [dxdt, dydt]
+    return dzdt
+
+#solve ODE
+t = np.linspace(0, 20, 200)
+x0 = 1 #initial x value
+y0 = 1 #initial y value
+z0 = [x0, y0]
+
+z = odeint(odesystem, z0, t)
+x = z[:, 0]
+y = z[:, 1]
+
+#plot results
+plt.plot(x, y)
+plt.show()
