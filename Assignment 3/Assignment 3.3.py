e9dc1193cf96ba33a977842e77ac4570b9f1a208
hannahchengg <hc200409@gmail.com>
2024-03-20 13:07:12 -0700

    mar 20

--- Assignment 3/Assignment 3.3.py
+++ Assignment 3/Assignment 3.3.py
@@ -14,13 +14,10 @@ from cycler import cycler
 
 df3 = pd.read_excel('Assignment 3/Assignment 3.xlsx', 'Problem 3')
 
-print(df3)
 v0 = df3["v for CI = 0"].values #mM/min
-v126 = df3["v for CI = 1.26"].values #mM/min
-v195 = df3["v for CI = 1.95"].values #mM/min
+v1 = df3["v for CI = 1.26"].values #mM/min
+v2 = df3["v for CI = 1.95"].values #mM/min
 Cs = df3["CS"].values #mM/min
-
-print(v0)
 
 #Define MM Equation
 def michaelis(substrate,params):
@@ -33,30 +30,48 @@ def residual0(params):
     return michaelis(Cs,params) - v0
 
 def residual1(params):
-    return michaelis(Cs,params) - v126
+    return michaelis(Cs,params) - v1
 
 def residual2(params):
-    return michaelis(Cs,params) - v195
+    return michaelis(Cs,params) - v2
 
 #Regression function
-fit0 = least_squares(residual0, [4,1]) #Fitting, [1,5] is the initial guess for params
-fit1 = least_squares(residual1, [7,1]) #Fitting, [1,5] is the initial guess for params
-fit2 = least_squares(residual2, [6,1]) #Fitting, [1,5] is the initial guess for params
+fit0 = least_squares(residual0, [5,1]) #Fitting, [1,5] is the initial guess for params
+fit1 = least_squares(residual1, [5,1]) #Fitting, [1,5] is the initial guess for params
+fit2 = least_squares(residual2, [5,1]) #Fitting, [1,5] is the initial guess for params
 
 velfit0 = michaelis(Cs, fit0.x) #Data for trendline, parameters stored in the x-array of fit2
 velfit1 = michaelis(Cs, fit1.x) #Data for trendline, parameters stored in the x-array of fit2
 velfit2 = michaelis(Cs, fit2.x) #Data for trendline, parameters stored in the x-array of fit2
 
 #Plot
-plt.plot(Cs, v0, "ko", label = 'v from experimental data')
-plt.plot(Cs, v126, "ko", label = 'v from experimental data')
-plt.plot(Cs, v195, "ko", label = 'v from experimental data')
-plt.plot(Cs, velfit0, "g", label = 'v from fit')
-plt.plot(Cs, velfit1, "g", label = 'v from fit')
-plt.plot(Cs, velfit2, "g", label = 'v from fit')
+plt.subplot(4,1,1)
+plt.plot(Cs, v0, "ko", label = 'v from data CI = 0')
+plt.plot(Cs, velfit0, "g", label = 'v from fit CI = 0')
 plt.xlabel("$C_S$ (M)")
 plt.ylabel('Reaction velocity (M/min)')
 plt.legend()
+plt.subplot(4,1,2)
+plt.plot(Cs, v1, "ko", label = 'v from data CI = 1.26')
+plt.plot(Cs, velfit1, "g", label = 'v from fit CI = 1.26')
+plt.xlabel("$C_S$ (M)")
+plt.ylabel('Reaction velocity (M/min)')
+plt.legend()
+plt.subplot(4,1,3)
+plt.plot(Cs, v2, "ko", label = 'v from data CI = 1.95')
+plt.plot(Cs, velfit2, "g", label = 'v from fit CI = 1.95')
+plt.xlabel("$C_S$ (M)")
+plt.ylabel('Reaction velocity (M/min)')
+plt.legend()
+
+plt.subplot(4,1,4)
+plt.plot(Cs, velfit0, "r", label = 'v from fit CI = 0')
+plt.plot(Cs, velfit1, "g", label = 'v from fit CI = 1.26')
+plt.plot(Cs, velfit2, "b", label = 'v from fit CI = 1.95')
+plt.xlabel("$C_S$ (M)")
+plt.ylabel('Reaction velocity (M/min)')
+plt.legend()
+plt.tight_layout()
 plt.show()
 
 vmax = fit0.x[0]
@@ -69,14 +84,42 @@ Km2 = fit2.x[1]
 
 t = np.linspace(0, 6)
 
-print('vmax estimated by non-linear regression is', vmax, 'M/min')
-print('vmax estimated by non-linear regression is', vmax1, 'M/min')
-print('vmax estimated by non-linear regression is', vmax2, 'M/min')
-print('Km estimated by non-linear regression is', Km, 'M')
-print('Km estimated by non-linear regression is', Km1, 'M')
-print('Km estimated by non-linear regression is', Km2, 'M')
+print('vmax estimated by non-linear regression is', vmax, 'mM/hr')
+print('vmax1 estimated by non-linear regression is', vmax1, 'mM/hr')
+print('vmax2 estimated by non-linear regression is', vmax2, 'mM/hr')
+print('Km estimated by non-linear regression is', Km, 'mM')
+print('Km1 estimated by non-linear regression is', Km1, 'mM')
+print('Km2 estimated by non-linear regression is', Km2, 'mM')
 
-#looking at equation:
-#v = vmax*Cs/(Km + Cs(1+Ci/Ki))
-#
-#therefore it is uncompetitive
+#Ci = 0 is the condition for regular MM equation
+
+#using uncompetitive equation:
+Ci = 0
+Ki = 0 
+Km0 = Km #this is the km of the original MM equation
+vmax0 = vmax #this is the vmax of the original MM equation
+
+#original equation: v = (vmax*S)/(Km + S)
+#uncompetitive equation: vuc = (vmax*Suc)/(Km + Suc*(1 + Iuc/Ki))
+
+#compare Vmax: Vmax0 = Vmax/(1+Ci/Ki)
+#compare Km: Km0 = Km/(1+Ci/Ki)
+
+Ci1 = 1.26
+Ci2 = 1.95
+
+#rearrange: 
+Ki1v = Ci1/((vmax1/vmax0) - 1)
+#rearrange: 
+Ki1k = Ci1/((Km1/Km0) - 1)
+
+print("Ki1v", Ki1v)
+print("Ki1k", Ki1k)
+
+#rearrange: 
+Ki2v = Ci2/((vmax2/vmax0) - 1)
+#rearrange: 
+Ki2k = Ci2/((Km2/Km0) - 1)
+
+print("Ki2v", Ki2v)
+print("Ki2k", Ki2k)
